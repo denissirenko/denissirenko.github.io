b6b23655569d08a73ab88cd57eab639a9d068b78
Denis Sirenko <sirenko.denis86@gmail.com>
2018-08-15 11:24:55 +0300

    first site

---
+++ js/init.js
@@ -0,0 +1,23 @@
+$( function() {
+    // init tabs
+    $( ".services-tab" ).tabs();
+    // init portfilio tabs
+    $( ".portfolio-tab" ).tabs();
+    // init reviews carousel
+    $( ".reviews-carousel" ).slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>'
+    });
+    // init slicknav
+    $('.header-nav').slicknav({
+        appendTo: '#header .container-fluid',
+        label: ''
+    });
+    // Wow init
+    new WOW().init();
+} );
+
+
